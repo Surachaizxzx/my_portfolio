2f8b89db1c629e81f71b3695120ca39ae938df75
surachai subnalsing <143887257+Surachaizxzx@users.noreply.github.com>
2024-10-25 17:18:11 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Section from './section';
+function App() {
+  return (
+    <>
+    <Section/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/section.js
@@ -0,0 +1,28 @@
+import React from "react";
+import './style/section.css'
+export default function Section() {
+    return (
+        <>
+            <div className='contrainer-section'>
+                <div className='item'>
+                    <div className="item-top">
+                        <div className="name">Surachai Subnalsing</div>
+                        <a className="button-name" href="http://localhost:3000/" >GitHub</a>  
+                    </div>
+                    <p >
+                            Web Developer
+                        </p>
+                    <div className="item-bottom">
+                        <div className="inside_top">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
+                        <p className="inside_bottom">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        
+                    </div>
+                </div>
+                <div className='item'>
+                    <div>fff</div>
+                </div>
+
+            </div>
+        </>
+    );
+}
