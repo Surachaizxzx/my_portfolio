5ceea9620f500a8b976810942a74acb4f796e2d5
surachai subnalsing <143887257+Surachaizxzx@users.noreply.github.com>
2024-10-26 18:18:55 +0700

    detail

--- src/article.js
+++ src/article.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React,{useState}from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import './style/article.css'
-export default  function Article(){
+const yolo = require('./images/YOLO.png')
+const shorturl = require('./images/shorturl.png')
+export default function Article() {
+    const [detail, setDetail]=useState('');
+    function clickinfo(e) {
+        eventprevent();
+
+        
+    }
     return (
         <>
-        <div className='grid-container'>
-        <div className='items-grid'>1</div>
-        <div className='items-grid'>2 </div>
-        <div className='items-grid'>3</div>
-        <div className='items-grid'>4</div>
-        </div>
+            <div className='grid-container'>
+                <div className='items-grid'><img src={yolo} alt='yolo' /><h5 >Strawberry Detection with YOLO V8</h5><div>Backend & frontend Developer</div><div className='detail-contrainer'><div className='linkgit'><a href='https://github.com/Surachaizxzx/MySynerry_shorturl' target="_blank" rel="noopener noreferrer">GitHub</a></div><div className='detail' onClick={(e)=>{clickinfo}}>More Detail</div></div></div>
+                <div className='items-grid shorturl'><img src={shorturl} alt='yolo' /><h5 className=''>Shorturl Website</h5><div>Backend & frontend Developer</div><div className='linkgit'><a href='https://github.com/Surachaizxzx/Strawberry_detection' target="_blank" rel="noopener noreferrer">GitHub</a></div></div>
+            </div>
         </>
     );
 }
