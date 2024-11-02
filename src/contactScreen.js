e06402f378497b33c0bde5fad32d5b048cffe0f8
surachai subnalsing <143887257+Surachaizxzx@users.noreply.github.com>
2024-11-02 23:23:38 +0700

    contact

--- src/contactScreen.js
+++ src/contactScreen.js
@@ -5,14 +5,22 @@ export default function Contact() {
         <>
             <div className="main_contact">
                 <div className="container_topic">
-                    <h1>Contact Me</h1>
+                    <h1>Contact</h1>
                 </div >
-                <div className="container_img"><img src={profile} alt="profile"></img></div>
-                <div className="contrainer_profileinfo">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum.</div>
-                <div className="contrainer_profileinfo">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum.</div>
-                <div className="contrainer_profileinfo">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum.</div>
-                <div className="contrainer_profileinfo">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum.</div>
-            </div>
+                <div className="container_img"><img src={profile} alt="profile"></img><div className="contact">
+                </div>
+                </div>
+                <div className="container_contact">
+                    <span className="contact_item"><div className="Inside_item"><i class="bi bi-telephone"></i>
+                        <p>Phone : +6640261977</p>
+                        <p>Email : surachaikai2545@gmail.com</p>
+                    </div></span>
+                    <span className="contact_item"><div className="Inside_item"><i class="bi bi-github"></i>
+                        <p>Phone : +6640261977</p>
+                        <p>Email : surachaikai2545@gmail.com</p>
+                    </div></span>
+                </div >
+            </div >
         </>
     )
 }
